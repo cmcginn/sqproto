201f5452d03e4a21c2b76051ee423540d621eca3
csmfdecisons <fdecisons2015>
2015-05-31 13:22:01 -0400

    square one

--- Squares/Scripts/app/square.js
+++ Squares/Scripts/app/square.js
@@ -1,194 +1,144 @@
 ﻿function Square(options) {
-    var postPath = rootPath + 'api/TimerAction';
-
-    function toTimerActionModel(state) {
-
-        return {
-            Time: new Date().getTime(),
-            Elapsed: result.timer.lap(),
-            ActivityState: state,
-            Id: result.activityId(),
-            ParentId: result.id(),
-            Modified: 0
-        };
+    var settings = options;
+    //need to resume
+    if (settings.StopWatch.State == 1) {
+        settings.StopWatch.Time = Date.now() - settings.StopWatch.Started;
     }
+    var t = common.getDuration(settings.StopWatch.Time);
 
-    var settings = options || {};
     var result = {
-        id: ko.observable(settings.Id),
-        activityId: ko.observable(settings.UserSquareActivityId),
-        elapsed: ko.observable(settings.Elapsed),
-        duration: ko.observable({
-            days: ko.observable(0),
-            hours: ko.observable(0),
-            minutes: ko.observable(0),
-            seconds: ko.observable(0),
-            milliseconds: ko.observable(0)
-        }),
-        name: ko.observable(settings.Name),
-        state: ko.observable(settings.ActivityState),
-        timer: null,
-        visible:ko.observable(true),
+        canHide: ko.observable(false),
         canReset: ko.observable(false),
-        canHide:ko.observable(false),
-        startTimer: function () {
-            result.timer.start(result.elapsed());
-            $.post(postPath, toTimerActionModel(1, 0), function (r) {
-                result.activityId(r.Id);
-                result.state(r.ActivityState);
-            });
+        duration: {
+            days: ko.observable(t.days),
+            hours: ko.observable(t.hours),
+            minutes: ko.observable(t.minutes),
+            seconds: ko.observable(t.seconds)
+        },
+        id: ko.observable(settings.Id),
+        name: ko.observable(settings.Name),
+        stopWatch: settings.StopWatch,
+        timer: null,
+        timerButtonDisplay: ko.observable(''),
+        visible: ko.observable(true),
 
-        },
-        pauseTimer: function () {
-            result.timer.pause();
-            $.post(postPath, toTimerActionModel(2, 0), function (r) {
-                result.activityId(r.Id);
-                result.state(r.ActivityState);
-            });
-        },
-        resumeTimer: function () {
-            if (result.timer.pause_time == 0) {
-                result.timer.start(result.elapsed());
-            } else
-                result.timer.pause();
-            $.post(postPath, toTimerActionModel(1, 0), function (r) {
-                result.activityId(r.Id);
-                result.state(r.ActivityState);
-            });
-        },
-        stopTimer: function () {
-
-            result.timer.stop();
-            $.post(postPath, toTimerActionModel(3, 0), function (r) {
-                result.activityId(r.Id);
-                result.state(r.ActivityState);
-                var d = common.getDuration(0);
-                result.duration().days(d.days);
-                result.duration().hours(d.hours);
-                result.duration().minutes(d.minutes);
-                result.duration().seconds(d.seconds);
-                result.duration().milliseconds(d.milliseconds);
-                result.elapsed(0);
-            });
-
-        },
-        toData: function () {
-            return {
-                UserSquareActivityId: result.activityId(),
-                Id: result.id(),
-                ActivityState: result.state(),
-                Elapsed: result.timer.lap(),
-                Time: new Date().getTime(),
-                Name: result.name()
-            };
-        },
-        init: function () {
-            var ms = result.elapsed();
-            var d = common.getDuration(ms);
-            result.duration().days(d.days);
-            result.duration().hours(d.hours);
-            result.duration().minutes(d.minutes);
-            result.duration().seconds(d.seconds);
-            result.duration().milliseconds(d.milliseconds);
-            result.timer = new Tock({ interval: 1000, callback: result.onTick });
-            if (result.state() == 1)
-                result.timer.start(result.elapsed());
-        },
         onDurationEdit: function () {
-            var el = result.timer.lap();
-       
-            if (result.state() != 0)
-                result.timer.stop();
-            
             var d = 0;
-            d += Number(result.duration().days()) * 86400000;
-            d += Number(result.duration().hours()) * 3600000;
-            d += Number(result.duration().minutes()) * 60000;
-            d += Number(result.duration().seconds()) * 1000;
-
-            result.elapsed(d);
-            var data = toTimerActionModel(result.state(), 0);
-            data.Elapsed = d;
-            data.Modified = el;
-            result.timer.start_time = new Date().getTime() - d;
-            $.post(postPath, data, function (r) {
-                if (result.state() != 0);
-                result.timer.start(result.elapsed());
-            });
-
-
+            d += Number(result.duration.days()) * 86400000;
+            d += Number(result.duration.hours()) * 3600000;
+            d += Number(result.duration.minutes()) * 60000;
+            d += Number(result.duration.seconds()) * 1000;
+            result.timer.modify(d);
+            result.save();
         },
-        onHideButtonClick: function () {
-            $.post(rootPath + 'home/HideUserSquare', { id: result.id() }, function(r) {
-                result.visible(false);
-            });
-        },
+        onHideButtonClick: function () { },
         onRenameClick: function () {
-            $.post(rootPath + 'Home/Rename', result.toData(), function (r, s) { });
-        },
-        onTick: function (e) {
-            var ms = result.timer.lap();
-            var d = common.getDuration(ms);
-            result.duration().days(d.days);
-            result.duration().hours(d.hours);
-            result.duration().minutes(d.minutes);
-            result.duration().seconds(d.seconds);
-            result.duration().milliseconds(d.milliseconds);
-
+            var d = result.data();
+            $.post(rootPath + 'api/Square', d, function (r) { });
         },
         onTimerButtonClick: function () {
-            switch (result.state()) {
+            switch (result.timer.state) {
                 case 0:
+                case 3:
                     result.startTimer();
+                    result.save();
                     break;
                 case 1:
                     result.pauseTimer();
+                    result.save();
                     break;
                 case 2:
-                    result.resumeTimer();
-                    break;
-                case 3:
                     result.startTimer();
+                    result.save();
                     break;
                 default:
                     break;
             }
+            result.updateState();
+            $.event.trigger({ type: 'TimerStateChanged', args: result, time: new Date() });
         },
         onTimerButtonResetClick: function () {
-            result.stopTimer();
+            result.timer.stop();
+            var d = result.timer.data();
+
+            $.post(rootPath + 'api/stopwatch', d, function (r) {
+                //do timer stuffs
+                result.timer.reset();
+                result.stopWatch = r;
+                result.timer.load(result.stopWatch);
+                result.refreshDuration(common.getDuration(0));
+                result.updateState(result.timer.state);
+            });
         },
+        onTimerTick: function () {
+            if (result.timer) {
+                var d = common.getDuration(result.timer.lap());
+                result.refreshDuration(d);
+            }
+
+        },
+
+        data: function () {
+            return {
+                StopWatch: result.timer.data(),
+                Name: result.name(),
+                Id: result.id()
+            };
+        },
+
+        init: function () {
+            var timer = new Tock({ interval: 1000, callback: result.onTimerTick });
+            timer.load(settings.StopWatch);
+            result.timer = timer;
+            result.updateState();
+        },
+        pauseTimer: function () {
+            result.timer.pause();
+        },
+        refreshDuration: function (d) {
+            result.duration.days(d.days);
+            result.duration.hours(d.hours);
+            result.duration.minutes(d.minutes);
+            result.duration.seconds(d.seconds);
+        },
+        save: function () {
+
+            var d = result.timer.data();
+            $.post(rootPath + 'api/stopwatch', d, function (r, s) { });
+        },
+        startTimer: function () {
+            result.timer.start();
+        },
+        updateState: function () {
+            switch (result.timer.state) {
+                case 0:
+                    result.timerButtonDisplay('Start');
+                    result.canReset(false);
+                    result.canHide(true);
+                    break;
+                case 1:
+                    result.timerButtonDisplay('Pause');
+                    result.canReset(false);
+                    result.canHide(false);
+                    break;
+                case 2:
+                    result.timerButtonDisplay('Resume');
+                    result.canReset(true);
+                    result.canHide(true);
+                    break;
+                case 3:
+                    result.timerButtonDisplay('Start');
+                    result.canReset(false);
+                    result.canHide(true);
+                    break;
+                default:
+                    break;
+
+            }
+        }
 
     };
 
-    result.timerButtonDisplay = ko.computed(function () {
-        var r = '';
-        switch (result.state()) {
-            case 0:
-                r = 'Start';
-                result.canReset(false);
-                result.canHide(true);
-                break;
-            case 1:
-                r = 'Pause';
-                result.canReset(false);
-                result.canHide(false);
-                break;
-            case 2:
-                r = 'Resume';
-                result.canReset(true);
-                result.canHide(true);
-                break;
-            case 3:
-                r = 'Start';
-                result.canReset(false);
-                result.canHide(true);
-                break;
-            default:
-                break;
-
-        }
-        return r;
-    }, this);
     result.init();
     return result;
 }
